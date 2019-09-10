065ad03fd7d112a094dd897a11909366c71efff3
Jonathan <jonathan.shifman@gmail.com>
2019-09-10 17:43:13 +0300

    PeriodFilter

---
+++ flashscore-crawler/PeriodFilter.py
@@ -0,0 +1,19 @@
+def year_in_period(year, period):
+    if len(period) == 1:
+        return year == period[0]
+    # len(period) == 2
+    return period[0] <= year <= period[1]
+
+
+def year_in_tournament_periods(year, tournament):
+    for period in tournament.periods:
+        if year_in_period(year, period):
+            return True
+    return False
+
+def filter_tournaments_by_year(tournaments, year):
+    filtered = []
+    for tournament in tournaments:
+        if year_in_tournament_periods(year, tournament):
+            filtered.append(tournament)
+    return filtered
--- flashscore-crawler/ScrapeYearEditions.py
+++ flashscore-crawler/ScrapeYearEditions.py
@@ -3,54 +3,42 @@ import pickle
 from utils import EditionSourceReader, Utils
 import time
 import os
-import traceback
+import PeriodFilter
 
 
-def year_in_period(year, period):
-    if len(period) == 1:
-        return year == period[0]
-    # len(period) == 2
-    return period[0] <= year <= period[1]
-
-
-def year_in_tournament_periods(year, tournament):
-    for period in tournament.periods:
-        if year_in_period(year, period):
-            return True
-    return False
-
-
+year = 2018
 tournaments = pickle.load(open('resources/periods.pkl', 'rb'))
+tournaments = PeriodFilter.filter_tournaments_by_year(tournaments, year)
 browser = Utils.get_browser()
-for year in range(2018, 2019):
-    for tournament in tournaments:
-        if year_in_tournament_periods(year, tournament):
-            dir_name = str(year) + "," + tournament.name
-            dir_path = 'output/editions/' + dir_name + '/'
-            if os.path.exists(dir_path + 'edition.pkl'):
-                continue
-            try:
-                print("Scraping " + str(year) + " " + tournament.name)
-                edition_string = tournament.name + '-' + str(year)
-                # results_source = EditionSourceReader.get_results_tab_source(browser, edition_string)
-                bracket_sources = EditionSourceReader.get_bracket_sources(browser, edition_string)
+for tournament in tournaments:
+    print("Scraping " + str(year) + " " + tournament.name)
+    dir_name = str(year) + "," + tournament.name
+    dir_path = 'output/editions/' + dir_name + '/'
+    if os.path.exists(dir_path + 'edition.pkl'):
+        print("Edition has already been scraped")
+        Utils.print_divider()
+        continue
+    try:
+        pass
+        edition_string = tournament.name + '-' + str(year)
+        results_source = EditionSourceReader.get_results_tab_source(browser, edition_string)
+        # bracket_sources = EditionSourceReader.get_bracket_sources(browser, edition_string)
 
-                if not os.path.exists(dir_path):
-                    os.makedirs(dir_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
 
-                # with open(dir_path + 'results.txt', 'wb') as output_file:
-                #     output_file.write(results_source.encode('utf8'))
+        # with open(dir_path + 'results.txt', 'wb') as output_file:
+        #     output_file.write(results_source.encode('utf8'))
 
-                bracket_serial_number = 1
-                for bracket_source in bracket_sources:
-                    with open(dir_path + 'bracket' + str(bracket_serial_number) + '.txt', 'wb') as output_file:
-                        output_file.write(bracket_source.encode('utf8'))
-                    bracket_serial_number += 1
-            except Exception as e:
-                print ("Failed to parse " + str(year) + " " + tournament.name + ": " + e.message)
-                # traceback.print_exc()
+        # bracket_serial_number = 1
+        # for bracket_source in bracket_sources:
+        #     with open(dir_path + 'bracket' + str(bracket_serial_number) + '.txt', 'wb') as output_file:
+        #         output_file.write(bracket_source.encode('utf8'))
+        #     bracket_serial_number += 1
+    except Exception as e:
+        print ("Failed to scrape " + str(year) + " " + tournament.name + ": " + e.message)
+        # traceback.print_exc()
 
-            Utils.print_divider()
-            Utils.print_divider()
-            time.sleep(5)
+    Utils.print_divider()
+    time.sleep(5)
 browser.quit()
