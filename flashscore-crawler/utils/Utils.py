1f53463a1975742a004a1209d889cb5671382ccd
Jonathan <jonathan.shifman@gmail.com>
2019-09-11 21:29:56 +0300

    disable-dev-shm-usage

--- flashscore-crawler/utils/Utils.py
+++ flashscore-crawler/utils/Utils.py
@@ -8,6 +8,7 @@ domain = "https://www.flashscore.com/"
 def get_browser(with_ui=False):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     if not with_ui:
         chrome_options.add_argument('headless')
     return webdriver.Chrome(chrome_options=chrome_options)
